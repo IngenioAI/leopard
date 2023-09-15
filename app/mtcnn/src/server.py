ec60e706e58c7020c3dd041367bea008f778d3f7
damaso98 <alex@ingenio.ai>
2023-09-15 13:57:39 +0900

    bug fix on face detection / anonymization

--- app/mtcnn/src/server.py
+++ app/mtcnn/src/server.py
@@ -2,23 +2,24 @@
     MTCNN module as server
 
 '''
-import os
 import argparse
-
-import uvicorn
-from fastapi import FastAPI, Request, Response, HTTPException
-from fastapi.responses import FileResponse, JSONResponse, HTMLResponse, RedirectResponse
-
-from mtcnn import MTCNN
-import cv2
-import json
 import copy
+import json
+import os
+from urllib import request
+
+import cv2
 import numpy as np
+import uvicorn
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from mtcnn import MTCNN
 
 app = FastAPI()
 
 
 def average_filter(mat, channels_first=True, alpha=False):
+    print("before average", mat)
     if channels_first:
         ch = 0
         new_mat = np.ones_like(mat, dtype=mat.dtype)
@@ -36,26 +37,34 @@ def average_filter(mat, channels_first=True, alpha=False):
             else:
                 new_mat[:, :, i] = np.ones_like(mat[:, :, i], dtype=mat.dtype) * np.mean(mat[:, :, i])
 
+    print("after average", new_mat)
     return new_mat
 
 
 def anonymize_faces(image_path, boxes, channels_first=False, alpha=False):
     mat = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+    print("boxes", boxes)
     new_mat = copy.deepcopy(mat)
 
     for box in boxes:
         x, y, width, height = box['box']
-        l = x - width // 2
-        r = x + width // 2
-        t = y - height // 2
-        b = y + height // 2
+        l = x
+        r = x + width
+        t = y
+        b = y + height
         if channels_first:
             blurred_face = average_filter(mat[:, t:b, l:r], channels_first=channels_first, alpha=alpha)
             new_mat[:, t:b, l:r] = blurred_face
         else:
             blurred_face = average_filter(mat[t:b, l:r, :], channels_first=channels_first, alpha=alpha)
             new_mat[t:b, l:r, :] = blurred_face
-    return new_mat
+
+    new_img = cv2.cvtColor(new_mat, cv2.COLOR_RGB2BGR)
+    new_img_name = "blurred_img.png"
+    new_img_path = '/data/input/' + new_img_name
+    cv2.imwrite(new_img_path, new_img)
+
+    return new_img_name
 
 
 def detect_faces(image_path):
@@ -64,11 +73,9 @@ def detect_faces(image_path):
     return detector.detect_faces(img)
 
 
-@app.get("/api/run/{param_path:path}")
-async def run_app(param_path: str):
-    with open("/data/input/%s" % param_path, "rt", encoding="UTF-8") as fp:
-        params = json.load(fp)
-
+@app.post("/api/run", tags=["App"])
+async def run_app(req: Request):
+    params = await req.json()
     if os.path.exists(params['input_filename']):
         image_path = params['input_filename']
     elif os.path.exists(os.path.join("/data/input", params['input_filename'])):
@@ -84,8 +91,9 @@ async def run_app(param_path: str):
         if mode == 'anonymize':
             result = {}
             box_res = detect_faces(image_path)
-            new_img = anonymize_faces(image_path, box_res)
-            result["new_image"] = new_img.tolist()
+            new_img_name = anonymize_faces(image_path, box_res)
+
+            result["new_image"] = new_img_name
             return JSONResponse(result)
 
         return JSONResponse({})
